83c35e3bb4942cc68968e5da0fa968d15ac740f4
Kristin Agcaoili <kagcaoil@ucsd.edu>
2017-01-14 13:47:31 -0700

    Wrote DetectScale.cs to scale up and down using up and down arrow.

---
+++ Assets/DetectScale.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectScale : MonoBehaviour {
+
+    private Vector3 currScale;
+    private Vector3 newScale;
+
+    [SerializeField] private float scaleUpFactor = 1.05f;
+    [SerializeField] private float scaleDownFactor = 0.95f;
+
+	// Use this for initialization
+	void Start () {
+        currScale = gameObject.transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.UpArrow))
+        {
+            //scale up
+            Debug.Log("Up Scale\n");
+            newScale = currScale * (1 + (scaleUpFactor * Time.deltaTime));
+            gameObject.transform.localScale = newScale;
+            currScale = newScale;
+        } else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            //scale down
+            Debug.Log("Down Scale\n");
+            newScale = currScale * (1 - (scaleDownFactor * Time.deltaTime));
+            gameObject.transform.localScale = newScale;
+            currScale = newScale;
+        }
+	}
+}
