4e4f34cdd873073283a68d3d06e7bc1aded8f265
Kristin Agcaoili <kagcaoil@ucsd.edu>
2017-01-14 22:13:01 -0700

    Attempted object grabbing. Error out of range in interact radial

--- Assets/Script/BeginScaleControl.cs
+++ Assets/Script/BeginScaleControl.cs
@@ -14,33 +14,37 @@ public class BeginScaleControl : MonoBehaviour {
     [SerializeField]
     private GameObject rightMenuPanel;
 
+    private bool hasScaled;
+
+    void Start()
+    {
+        hasScaled = false;
+    }
+
+    public void SetCheckForHasScaled(bool status)
+    {
+        hasScaled = status;
+    }
+
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("ontriggerenter\n");
         if (other.gameObject.CompareTag("MainCamera"))
         {
-            Debug.Log("detected maincamera entering\n");
             SetRadialPanelsActive(true);
         }
     }
 
     void OnTriggerExit(Collider other)
     {
-        Debug.Log("oncollisionexit\n");
         if (other.gameObject.CompareTag("MainCamera"))
         {
-            Debug.Log("detected maincamera exiting\n");
             SetRadialPanelsActive(false);
-        } else
-        {
-            Debug.Log("the thing leaving is: " + other.gameObject.name);
-        }
+        } 
     }
 
     void SetRadialPanelsActive(bool active)
     {
-        Debug.Log("settign radial panels " + active + "\n");
-        if (!active)
+        if (!active && hasScaled)
         {
             leftMenuPanel.GetComponent<RadialMenu>().UnClickButton(351.6f);
             rightMenuPanel.GetComponent<RadialMenu>().UnClickButton(174.8f);
