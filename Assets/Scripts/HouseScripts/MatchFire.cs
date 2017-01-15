ba96e16d3b7466ec1fdf879d8ab7a49fcdda63b8
Ani Kan <Ani Kan>
2017-01-14 16:07:37 -0800

    Added a few interactable objects

---
+++ Assets/Scripts/HouseScripts/MatchFire.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class MatchFire : VRTK_InteractableObject {
+
+    public GameObject fireParticles;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void StartFire()
+    {
+        fireParticles.SetActive(true);
+    }
+
+    public override void StartUsing(GameObject usingObject)
+    {
+        base.StartUsing(usingObject);
+        StartFire();
+    }
+}
---
+++ Assets/Scripts/HouseScripts/RoofFire.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class RoofFire : MonoBehaviour {
+
+    public float roofBurnTime = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.GetComponent<MatchFire>())
+        {
+
+
+
+        }
+    }
+
+    private IEnumerator SetRoofOnFire()
+    {
+
+        //Set all fires active.
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(true);
+        }
+
+        //Wait for a few seconds, then destroy the roof.
+        yield return new WaitForSeconds(roofBurnTime);
+        GameObject.Destroy(this.gameObject);
+    }
+}
