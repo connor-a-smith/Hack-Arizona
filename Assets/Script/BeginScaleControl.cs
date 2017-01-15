0e1083e2309f39d3b80e37e5b9d8be5b142ab1db
Kristin Agcaoili <kagcaoil@ucsd.edu>
2017-01-14 17:16:48 -0700

    Activates scaling if you enter the green beam.

---
+++ Assets/Script/BeginScaleControl.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class BeginScaleControl : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject leftRadial;
+    [SerializeField]
+    private GameObject rightRadial;
+    [SerializeField]
+    private GameObject leftMenuPanel;
+    [SerializeField]
+    private GameObject rightMenuPanel;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("ontriggerenter\n");
+        if (other.gameObject.CompareTag("MainCamera"))
+        {
+            Debug.Log("detected maincamera entering\n");
+            SetRadialPanelsActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("oncollisionexit\n");
+        if (other.gameObject.CompareTag("MainCamera"))
+        {
+            Debug.Log("detected maincamera exiting\n");
+            SetRadialPanelsActive(false);
+        } else
+        {
+            Debug.Log("the thing leaving is: " + other.gameObject.name);
+        }
+    }
+
+    void SetRadialPanelsActive(bool active)
+    {
+        Debug.Log("settign radial panels " + active + "\n");
+        if (!active)
+        {
+            leftMenuPanel.GetComponent<RadialMenu>().UnClickButton(351.6f);
+            rightMenuPanel.GetComponent<RadialMenu>().UnClickButton(174.8f);
+        }
+        leftRadial.SetActive(active);
+        rightRadial.SetActive(active);
+
+        
+    }
+}
